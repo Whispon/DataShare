ab8fae5bf13aad21a1168fa521fdf6a48005ad7a
oshita-n <oshita-n@whispon.com>
2022-06-29 02:38:19 +0900

    🔨 データ検索画面の追加

--- src/pages/search.tsx
+++ src/pages/search.tsx
@@ -7,21 +7,17 @@ const Home: NextPage = () => {
     <div>
       <Header />
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex flex-col items-center">
-          <div className="mb-6 text-4xl flex flex-row">
-            <Link href="/preview">
-              Import CSV
-            </Link>
-          </div>
-          <div className="flex flex-row">
+        <div className="flex flex-row mb-6">
             <form>
               <input type="text" name="name" className='w-96 border border-gray-300 p-2.5'/>
             </form>
-          </div>
-          <div className="mt-6 text-4xl flex flex-row">
-            <Link href="/search">
-              データを検索
-            </Link>
-          </div>
+        </div>
+
+        <div className="flex flex-row">
+            <div className='w-80 h-60 bg-orange-400'>
+                CSVリストのイメージ
+            </div>
+        </div>
       </div>
     </div>
   )
