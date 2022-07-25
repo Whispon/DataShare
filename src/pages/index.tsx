b64b77d6be79fa7277b106140de462c2dca2da7b
oshita-n <oshita-n@whispon.com>
2022-07-26 00:19:17 +0900

    🔨 サイドバーにボタンの追加

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import type { NextPage } from 'next'
+import React from 'react';
 import { Header } from '../components/Header'
+import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
   return (
@@ -9,11 +11,14 @@ const Home: NextPage = () => {
       <div className="flex flex-row grow">
           {/* sidebar */}
           <div className="w-36 bg-gray-100 border-r border-gray-300">
+            <div className="flex flex-col">
+              <button className="">sample1</button>
+              <button className="">sample2</button>
+            </div>
           </div>
 
           {/* content */}
-          <canvas className="bg-sky-500">
-            
+          <canvas className="bg-sky-200">
           </canvas>
       </div>
     </div>
