85a97f02feeccfb0e0fde754e7e2bcf68f7dc9c3
oshita-n <oshita-n@whispon.com>
2022-09-28 00:42:58 +0900

    ✨ ログイン機能、ユーザー登録機能の作成

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,59 @@
 import Link from 'next/link'
+import { useUser } from '@auth0/nextjs-auth0';
 
 export const Header: React.FC = () => {
+  const { user, error, isLoading } = useUser();
+
+  if (isLoading) return (
+    <div className="h-10 border-b border-gray-300">
+        <div className="text-2xl text-left ml-5">
+          <div className='flex flex-row'>
+            <Link href="/">
+              Demiurge4D
+            </Link>
+            <div>Loading...</div>
+          </div>
+        </div>
+      </div>
+  );
+
+  if (error) return (
+    <div className="h-10 border-b border-gray-300">
+        <div className="text-2xl text-left ml-5">
+          <div className='flex flex-row'>
+            <Link href="/">
+              Demiurge4D
+            </Link>
+            <div>{error.message}</div>
+          </div>
+        </div>
+      </div>
+  );
+
+  if (user) {
+    return (
+      <div className="h-10 border-b border-gray-300">
+        <div className="text-2xl text-left ml-5">
+          <div className='flex flex-row'>
+            <Link href="/">
+              Demiurge4D
+            </Link>
+            Welcome {user.name}!  <Link href="/api/auth/logout">Logout</Link>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="h-10 border-b border-gray-300">
       <div className="text-2xl text-left ml-5">
-        <Link href="/">
-        Demiurge4D
-        </Link>
+        <div className='flex flex-row'>
+          <Link href="/">
+            Demiurge4D
+          </Link>
+          <Link href="/api/auth/login">Login</Link>
+        </div>
       </div>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,9 @@ import { useLoader } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 import { Suspense } from "react";
-import Link from 'next/link'
+import Link from 'next/link';
+import { useUser } from '@auth0/nextjs-auth0';
+
 
 // function Scene() {
 //   const fbx = useFBX('casper-octpus-0209-b.fbx')
@@ -19,8 +21,11 @@ function Scene() {
 }
 
 const Home: NextPage = () => {
-  return (
-    <div className="flex flex-col w-screen h-screen">
+  const { user, error, isLoading } = useUser();
+
+  if (user) {
+    return (
+      <div className="flex flex-col w-screen h-screen">
       <Header />
       {/* container */}
       <div className="flex flex-row grow">
@@ -39,6 +44,31 @@ const Home: NextPage = () => {
               <OrbitControls />
             </Suspense>
           </Canvas>
+      </div>
+    </div>
+    );
+  };
+  return (
+    <div className="flex flex-col w-screen h-screen">
+      <Header />
+      {/* container */}
+      <div className="flex flex-row grow">
+          {/* sidebar */}
+          <div className="w-36 bg-gray-100 border-r border-gray-300 h-screen">
+            <div className="flex flex-col">
+              <Link className="text-center" href="/products">products</Link>
+            </div>
+          </div>
+
+          {/* content */}
+          <h1>ログインしていません。ログインしてください。</h1>
+          {/* <Canvas className="h-screen">
+            <color attach="background" args={['#ababab']} />
+            <Suspense fallback={null}>
+              <Scene />
+              <OrbitControls />
+            </Suspense>
+          </Canvas> */}
       </div>
     </div>
   )
